200083de85eec45f244c4e00aba8294d39744738
Seungwon-Choi <bestwondev@gmail.com>
2025-04-09 09:11:34 +0900

    initial commit

---
+++ src/test/java/com/backendstudyweek2/BackendStudyWeek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendstudyweek2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStudyWeek2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
